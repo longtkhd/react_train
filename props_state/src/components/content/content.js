046e274e6fc18565c353187076c4a5949281280c
longtkhd <longtkhd2709@gmail.com>
2019-09-26 16:44:02 +0700

    add state edit and save

--- props_state/src/components/content/content.js
+++ props_state/src/components/content/content.js
@@ -1,6 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+
+
 
 export default class Content extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            trangthai: 0
+        }
+    }
+    renderButton = () => {
+        return (
+        < div className="row" >
+            <div className="btn btn-group" >
+                <div className="btn btn-info" onClick={()=>this.editClick()} > Edit </div>
+                <div className="btn btn-warning" > Remove </div>
+            </div> </div>
+    )}
+    renderForm = () => (
+        < div className="row" >
+            <div className="form-grounp" >
+                <input type="text" />
+                <div className="btn btn-block btn-danger" onClick={() => this.saveClick()}> Save </div> </div>
+        </div>
+    )
+    
+
+
+    displayCheck = () => {
+        if (this.state.trangthai === 0) {
+            return this.renderButton();
+        }else 
+        return this.renderForm();
+    }
+
+    editClick = () => {
+        this.setState({trangthai:1});
+    }
+
+    saveClick = () => {
+        this.setState({ trangthai: 0 });
+    }
+
     render() {
         return (
             <section>
@@ -15,7 +58,8 @@ export default class Content extends Component {
                             <div className="p-5">
                                 <h2 className="display-4">{this.props.tieude}</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-
+                                    {this.displayCheck()}
+                                     
                             </div>
                         </div>
                     </div>
